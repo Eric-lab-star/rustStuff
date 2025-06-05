9caca4f79de34a593d5fd6306d25ab235b434149
IDeal <cyon256@icloud.com>
2025-06-06 06:34:28 +0900

    leet code: remove elements

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 
 fn main() {
-    let mut nums = vec![1, 1, 2, 2, 3, 3];
-    let a = Solution::remove_duplicates(&mut nums);
-    println!("{a:?}");
+    let mut nums = vec![3, 2, 2, 3];
+    let a = Solution::remove_element(&mut nums, 3);
+    println!("nums: {:?}, a: {a}", nums);
+
 }
 struct Solution;
 
 impl Solution {
-   pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-       if nums.is_empty() {
-           return 0;
-       }
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut k = 0;
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+        k as i32
+    }
 
-       let mut j = 0;
-       for i in 1..nums.len() {
-           if nums[j] != nums[i] {
-               j += 1;
-               nums[j] = nums[i];
-           }
-       }
-
-       (j + 1) as i32
-   } 
 }
 
 
