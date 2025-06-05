55d3578d5c9692fb988489b05ad8158daf9d4dd4
IDeal <cyon256@icloud.com>
2025-06-06 07:17:28 +0900

    Leetcode search insert position

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 
 fn main() {
-    let mut nums = vec![3, 2, 2, 3];
-    let a = Solution::remove_element(&mut nums, 3);
-    println!("nums: {:?}, a: {a}", nums);
+    let nums = vec![1,3,5,6];
+    let a = Solution::search_insert(nums, 4);
+    println!("a: {a}");
 
 }
 struct Solution;
 
 impl Solution {
-    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        let mut k = 0;
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut k = 0;   
         for i in 0..nums.len() {
-            if nums[i] != val {
-                nums[k] = nums[i];
-                k += 1;
+            match nums[i] {
+                n if n >= target => {
+                    return k;
+                },
+                n if n < target => {
+                    k += 1;
+                },
+                _ => {}
             }
         }
-        k as i32
+        k
     }
 
 }
