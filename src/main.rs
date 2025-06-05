014ca5d927586af5a5c38f10587a627462ece624
IDeal <cyon256@icloud.com>
2025-06-06 08:07:09 +0900

    Leetcode plus one

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
 
 fn main() {
-    let nums = vec![1,3,5,6];
-    let a = Solution::search_insert(nums, 4);
-    println!("a: {a}");
+    let nums = vec![9,9,9];
+    let a = Solution::plus_one(nums);
+    println!("a: {a:?}");
 
 }
 struct Solution;
 
 impl Solution {
-    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        let mut k = 0;   
-        for i in 0..nums.len() {
-            match nums[i] {
-                n if n >= target => {
-                    return k;
-                },
-                n if n < target => {
-                    k += 1;
-                },
-                _ => {}
-            }
+    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+        let mut p = digits.len() - 1;
+        let mut num = digits[p];
+        let mut tens = 0;
+        num += 1;
+        if num >= 10 {
+            num = 0;
+            tens = 1;
         }
-        k
+
+        let mut new_digits = vec![0; digits.len()];
+        new_digits[p] = num;
+        p -= 1;
+
+        for _ in 1..digits.len() {
+            let mut num = digits[p];
+            if tens != 0 {
+                num += tens;
+                tens = 0;
+            }
+            if num >= 10 {
+                tens += 1;
+                num %= 10;
+            }
+            new_digits[p] = num;
+            if p != 0{
+                p = p.saturating_sub(1);
+            }
+
+        }
+        if tens != 0 {
+            new_digits.insert(0, 1);
+        }
+
+        new_digits
     }
 
 }
 
+// 9 -> 10
+// 99 -> 100
+// 299 -> 1000
+
+// 29 -> 30
 
