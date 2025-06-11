af73ce2319dcf2441a8f0e7a4ca558145a440ed2
IDeal <cyon256@icloud.com>
2025-06-11 19:45:28 +0900

    Leetcode Pascal's triangle 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 
+
 fn main() {
-    let num_rows = 5;
-    let a = Solution::generate(num_rows);
+    let num_rows = 3;
+    let a = Solution::get_row(num_rows);
     println!("{a:?}");
 }
 
@@ -9,31 +10,31 @@ fn main() {
 struct Solution;
 
 impl Solution {
-    pub fn generate(num_rows: i32)
-    -> Vec<Vec<i32>> {
-        if num_rows == 1 {
-            return vec![vec![1]];
+    pub fn get_row(row_index: i32)
+    -> Vec<i32> {
+        if row_index == 0 {
+            return vec![1];
         }
-        if num_rows == 2 {
-           return vec![vec![1], vec![1, 1]]; 
+        if row_index == 1 {
+           return vec![1, 1];
         }
-        let mut outer: Vec<Vec<i32>> = vec![vec![1], vec![1, 1]];
-        for i in 2..num_rows {
-            let prev_row = &outer[(i - 1) as usize];
-            let mut inner: Vec<i32> = vec![];
+        let mut prev_row = vec![1, 1];
+        for _ in 2..row_index + 1 {
+            let mut tmp = vec![];
             for j in 0..prev_row.len() {
                 if j + 1 > prev_row.len() - 1 {
                     break;
                 }
                 let a = prev_row[j];
                 let b = prev_row[j + 1];
-                inner.push(a + b);
+                tmp.push(a + b);
             }
-            inner.insert(0, 1);
-            inner.push(1);
-            outer.push(inner);
+            prev_row = tmp;
+            prev_row.insert(0, 1);
+            prev_row.push(1);
         }
-        outer
+        prev_row
+
     }
 
 }
