f97b71832b1e3b6ee7c3e83635d21a6b1bd134a1
IDeal <cyon256@icloud.com>
2025-06-13 06:22:28 +0900

    leetcode Best time to buy and sell stock

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,27 @@
-
-
 fn main() {
-    let num_rows = 3;
-    let a = Solution::get_row(num_rows);
-    println!("{a:?}");
+    let prices = vec![7,1,5,3,6,4];
+    let a = Solution::max_profit(prices);
+    println!("{a}");
+
 }
 
 
 struct Solution;
 
 impl Solution {
-    pub fn get_row(row_index: i32)
-    -> Vec<i32> {
-        if row_index == 0 {
-            return vec![1];
-        }
-        if row_index == 1 {
-           return vec![1, 1];
-        }
-        let mut prev_row = vec![1, 1];
-        for _ in 2..row_index + 1 {
-            let mut tmp = vec![];
-            for j in 0..prev_row.len() {
-                if j + 1 > prev_row.len() - 1 {
-                    break;
-                }
-                let a = prev_row[j];
-                let b = prev_row[j + 1];
-                tmp.push(a + b);
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut buy = prices[0];
+        let mut profit = 0;
+
+        for &price in prices.iter().skip(1) {
+            if price < buy {
+                buy = price; 
+            } else if price - buy > profit {
+                profit = price - buy;
             }
-            prev_row = tmp;
-            prev_row.insert(0, 1);
-            prev_row.push(1);
         }
-        prev_row
-
+        profit
+        
     }
-
 }
 
