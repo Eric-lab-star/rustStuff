217e9d7608826be8df777b2d2960c4ad274662eb
IDeal <cyon256@icloud.com>
2025-06-15 07:09:27 +0900

    leetcode Summary Ranges

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-use std::collections::HashMap;
-
+// Summary Ranges
 
 fn main() {
-    let nums = vec![99,99];
-    let a = Solution::contains_nearby_duplicate(nums, 2);
-    println!("{a}");
+    let nums = vec![];
+    let ans = Solution::summary_ranges(nums);
+    println!("{ans:?}");
 }
 
 
 struct Solution;
 
 impl Solution {
-    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
-       let mut last_index = HashMap::new();
-       for (i, num) in nums.iter().enumerate() {
-           if matches!(last_index.insert(num, i), Some(j) if i.abs_diff(j) <= k as usize ){
-               return true;
-           }
-       }
-       false
+    pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
+        if nums.is_empty() {
+            return vec![];
+        }
+        let mut start: i32 = nums[0];
+        let mut ans: Vec<String> = vec![];
+        if nums.len() < 2 {
+            ans.push(format!("{start}"));
+            return ans;
+        }
+
+        for (i, &num) in nums.iter().enumerate().skip(1) {
+            if num != nums[i - 1] + 1 {
+                if start == nums[i - 1] {
+                    ans.push(format!("{start}"));
+                } else {
+                    ans.push(format!("{start}->{}", nums[i - 1]));
+                }
+                start = nums[i];
+            }
+            if i == nums.len() - 1 {
+                if start != nums[i] {
+                    ans.push(format!("{start}->{}", nums[i]));
+                } else {
+                    ans.push(format!("{start}"));
+                } 
+            }
+        }
+        ans
     }
 }
