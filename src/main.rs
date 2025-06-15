b89e6d1abe1f5e594ad6b8ae2f3e022bc6d85b81
IDeal <cyon256@icloud.com>
2025-06-16 06:26:35 +0900

    leetcode Move Zeroes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-// Missing Number
+// Move Zeros
 
 fn main() {
-    let nums = vec![9,6,4,2,3,5,7,0,1];
-    let ans = Solution::missing_number(nums);
-    println!("{ans:?}");
+    let mut nums: Vec<i32> = vec![0,1,0,3,12];
+    Solution::move_zeroes(&mut nums);
+    println!("{nums:?}");
+
 }
 
 
 struct Solution;
 
 impl Solution {
-    pub fn missing_number(nums: Vec<i32>)
-    -> i32 {
-        nums.into_iter()
-            .enumerate()
-            .fold(0, |acc, (i, x)| acc ^ (i + 1) as i32 ^ x)
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut not_zero = 0; 
+        for j in 0..nums.len() {
+            if nums[j] != 0 {
+                nums[not_zero] = nums[j];
+                not_zero += 1;
+            }
+        }
+        for el in nums.iter_mut().skip(not_zero) {
+            *el = 0;
+        }
     }
+    
 }
-
-
