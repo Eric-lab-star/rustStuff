24a8b109590200126cfd36750aab333e43b61f81
IDeal <cyon256@icloud.com>
2025-06-24 00:33:42 +0900

    Next Greater Element 1

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
-// teemo attacking 
+// Next Greater Element 1
+
 
 fn main() {
-   let timeSeries = vec![1,4];
-   let duration = 2; 
-   let ans = Solution::find_poisoned_duration(timeSeries, duration);
-   println!("{ans}");
+    let nums1 = vec![2,4];
+    let nums2 = vec![1,2,3,4];
+    let ans = Solution::next_greater_element(nums1, nums2);
+    assert_eq!(ans, [3,-1]);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
-        time_series.iter().enumerate().skip(1).fold(duration, |acc, (index, &time)| {
-            let interval = time - time_series[index - 1];
-            if interval > duration {
-                duration + acc
-            } else {
-                interval + acc
-            }
-        })
+    pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut stack = Vec::new();
+        let mut answer:Vec<i32> = vec![-1; nums1.len()];
 
+        for i in 0..nums2.len() {
+            while stack.last().map_or(false, |&l| l < nums2[i]) {
+                let val = stack.pop().unwrap();
+                if let Some(j) = nums1.iter().position(|&n| n == val) {
+                    answer[j] = nums2[i];
+                }
+            }
+            stack.push(nums2[i]);
+        }
+        answer
     }
 }
